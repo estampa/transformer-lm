a133965a314aea094f196974e342df10a9cf726e
Pau Artigas <pau@tallerestampa.com>
2019-11-29 15:42:36 +0100

    Oferim l'opcio per guardar el resultat en fitxers

--- lm/generate.py
+++ lm/generate.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import re
+import time
 
 import fire
 
@@ -25,7 +26,7 @@ def find_similar(text, folder):
                 # print("--\n" + linia + "\n" + closest + "\n" + closest_file[:48] + " - " + str(closest_dist))
 
 
-def gen_main(model_path, prefix, tokens_to_generate=42, top_k=32, temperature=0.8, num=1, context="", originals=""):
+def gen_main(model_path, prefix, tokens_to_generate=42, top_k=32, temperature=0.8, num=1, context="", save=False, originals=""):
     print(f'loading model from {model_path}')
     mw = ModelWrapper.load(Path(model_path))
 
@@ -36,12 +37,18 @@ def gen_main(model_path, prefix, tokens_to_generate=42, top_k=32, temperature=0.
         context_tokens = []
     tokens = [END_OF_TEXT] + context_tokens + mw.tokenize(str(prefix))
 
-    for _ in range(0,num):
+    filename_base = "gpt2-{}-{}".format(model_path.replace("/", "_"), time.strftime("%Y%m%d-%H%M"))
+
+    for i in range(0,num):
         tokens_gen = mw.generate_tokens(tokens, tokens_to_generate, top_k, temperature)
         # print(mw.sp_model.DecodePieces(tokens_gen))  # No mostra salts de línia
         text_gen = "".join(tokens_gen[len(context_tokens):]).replace("▁", " ").replace(END_OF_LINE, "\n").replace(END_OF_TEXT, "")
         print("----")
         print(text_gen)
+        if save:
+            filename = "{}-{}.txt".format(filename_base, str(i).zfill(2))
+            with open(filename, "w") as output_file:
+                output_file.write(text_gen)
 
         if originals:
             find_similar(text_gen[len(context_tokens):], originals)

8130eed9de7b4a400783c60790f9d0d7c25b2bb4
Pau Artigas <pau.artigas@gmail.com>
2021-01-29 16:52:35 +0100

    Afegim funcions per processar datasets on els textos són línies en un o varis fitxers

--- lm/data.py
+++ lm/data.py
@@ -1,6 +1,7 @@
 import argparse
 from collections import defaultdict
 from pathlib import Path
+import random
 
 import numpy as np
 import sentencepiece as spm
@@ -34,6 +35,59 @@ def sp_train():
             corpus_paths = list(train_root.glob('**/*.txt'))
             if not corpus_paths:
                 parser.error(f'Corpus train split {train_root} looks empty, '
+                             f'no text files found')
+            paths.extend(corpus_paths)
+        try:
+            with sp_text.open('wt', encoding='utf8') as sp_text_file:
+                for path in tqdm.tqdm(
+                        paths, desc='building sentencepiece input'):
+                    with path.open('rt', encoding='utf8') as f:
+                        for line in f:
+                            if line.strip():
+                                sp_text_file.write(line)
+        except Exception:
+            if sp_text.exists():
+                sp_text.unlink()
+            raise
+
+    spm.SentencePieceTrainer.train(' '.join([
+        f'--input={sp_text}',
+        f'--model_prefix={args.sp_model_prefix}',
+        f'--vocab_size={args.vocab_size}',
+        f'--model_type=bpe',
+        f'--max_sentence_length=16384',
+        f'--bos_id=-1',
+        f'--eos_id=-1',
+        f'--unk_piece={UNK}',
+        f'--control_symbols={END_OF_LINE},{END_OF_TEXT}',
+        f'--character_coverage={args.character_coverage}',
+    ]))
+
+
+def sp_train_lines():
+    # TODO consider moving to fire
+    parser = argparse.ArgumentParser(
+        description='build sentencepiece model on all the subset of the corpora')
+    arg = parser.add_argument
+    arg('corpora', nargs='+',
+        help='corpus roots, containing train/valid/test splits')
+    arg('sp_text', help='text file for sentencepiece model '
+                        '(will be used as-is if exists)')
+    arg('sp_model_prefix', help='path (prefix) to output sentencepiece model')
+    arg('--vocab-size', type=int, default=50000)
+    arg('--character-coverage', type=float, default=1.0)
+    args = parser.parse_args()
+
+    sp_text = Path(args.sp_text)
+    if sp_text.exists():
+        print(f'Using existing "{sp_text}", remove and re-run if it is stale.')
+    else:
+        paths = []
+        print(f'Reading corpora: {args.corpora}')
+        for corpus_root in map(Path, args.corpora):
+            corpus_paths = list(corpus_root.glob('**/*.txt'))
+            if not corpus_paths:
+                parser.error(f'Corpus {corpus_root} looks empty, '
                              f'no text files found')
             paths.extend(corpus_paths)
         try:
@@ -105,6 +159,71 @@ def sp_encode():
                             append_and_clear(encoded)
                     encoded.append(eot)
                 append_and_clear(encoded)
+
+    output_root = Path(args.output)
+    output_root.mkdir(exist_ok=True, parents=True)
+    for split in ['train', 'valid', 'test']:
+        split_path = output_root / f'{split}.npy'
+        print(f'Saving encoded split {split} to {split_path}')
+        encoded = np.concatenate(encoded_splits[split])
+        assert encoded.dtype == dtype
+        np.save(split_path, encoded)
+
+
+def sp_encode_lines():
+    parser = argparse.ArgumentParser(
+        description='encode corpus with a sentencepiece model')
+    arg = parser.add_argument
+    arg('corpora', nargs='+',
+        help='corpus root conatining text files')
+    arg('sp_model', help='path to output model')
+    arg('output', help='path to the output directory, '
+                       'which will contain train.npy, valid.npy and test.npy')
+    arg('train', type=int, help='train percentage', default=90)
+    arg('valid', type=int, help='test percentage', default=10)
+    arg('test', type=int, help='train percentage', default=0)
+    args = parser.parse_args()
+
+    total_pct = args.train + args.valid + args.test
+    train_pct = args.train / total_pct
+    valid_pct = args.valid / total_pct
+    test_pct = args.test / total_pct
+
+    sp_model = spm.SentencePieceProcessor()
+    assert sp_model.load(args.sp_model)
+    eot = sp_model.PieceToId(END_OF_TEXT)
+    eol = sp_model.PieceToId(END_OF_LINE)
+    dtype = np.uint16 if len(sp_model) < 2**16 - 1 else np.uint32
+
+    print(f'Reading corpora: {args.corpora}')
+    encoded_splits = defaultdict(list)
+    for corpus_root in map(Path, args.corpora):
+        corpus_paths = list(corpus_root.glob('**/*.txt'))
+        if not corpus_paths:
+            parser.error(f'Corpus {corpus_root} looks empty, '
+                         f'no text files found')
+
+        def append_and_clear(x, split):
+            encoded_splits[split].append(np.array(x, dtype=dtype))
+            x.clear()
+
+        for path in tqdm.tqdm(corpus_paths, desc=str(corpus_root)):
+            encoded = []
+            with path.open('rt', encoding='utf8') as f:
+                for line in f:
+                    encoded.extend(sp_model.EncodeAsIds(line))
+                    encoded.append(eol)
+                    encoded.append(eot)
+
+                    rnd = random.random()
+                    if rnd < train_pct:
+                        split = 'train'
+                    elif rnd < train_pct + valid_pct:
+                        split = 'valid'
+                    else:
+                        split = 'test'
+
+                    append_and_clear(encoded, split)
 
     output_root = Path(args.output)
     output_root.mkdir(exist_ok=True, parents=True)
--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(
     entry_points={
         'console_scripts': [
             'sp-train = lm.data:sp_train',
+            'sp-train-lines = lm.data:sp_train_lines',
             'sp-encode = lm.data:sp_encode',
+            'sp-encode-lines = lm.data:sp_encode_lines',
             'gpt-2-tf-train = lm.gpt_2_tf.train:main',
             'gpt-2 = lm.main:fire_main',
             'gpt-2-gen = lm.generate:fire_gen_main',
